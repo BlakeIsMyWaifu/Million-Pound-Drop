4cd67d748bdbd88eafa48a081e7cf2f3b9ef934a
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2023-10-21 23:16:22 +0100

    📦 eslint + prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,26 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/strict-type-checked',
+		'plugin:react-hooks/recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'prettier'
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh', 'simple-import-sort'],
+	rules: {
+		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+		'simple-import-sort/imports': 'warn',
+		'simple-import-sort/exports': 'warn',
+		'@typescript-eslint/no-unused-vars': ['warn', { vars: 'all', args: 'after-used', argsIgnorePattern: '^_' }],
+		'@typescript-eslint/no-empty-function': 'warn',
+		'@typescript-eslint/no-empty-interface': 'warn',
+		'@typescript-eslint/consistent-type-imports': ['warn', { fixStyle: 'inline-type-imports' }
+		]
+	}
 }
